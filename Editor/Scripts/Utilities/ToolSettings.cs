917bba8af7c6f2b9ff2a2efa5572873dd985467e
Jon McCreadie <mccducky23@gmail.com>
2025-04-09 20:05:35 +0100

    Add field to track if Grid Snapping is active.

--- Editor/Scripts/Utilities/ToolSettings.cs
+++ Editor/Scripts/Utilities/ToolSettings.cs
@@ -26,6 +26,7 @@ namespace PrefabPalette
         public float brokenProbability = 0.5f;
         public int brokenInterval = 4;
         public bool showModeSettings;
+        public bool enableGridSnap = false;
 
         // Marked dirty on disable so Unity knows to save it
         private void OnDisable()
