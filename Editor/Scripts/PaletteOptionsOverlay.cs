072e19483b38e09733df59e0db9e5b244c969b6d
Jon McCreadie <mccducky23@gmail.com>
2025-05-13 22:08:56 +0100

    Add: Scroll view & conditional open palette button

--- Editor/Scripts/PaletteOptionsOverlay.cs
+++ Editor/Scripts/PaletteOptionsOverlay.cs
@@ -9,33 +9,51 @@ namespace PrefabPalette
     [Overlay(typeof(SceneView), "Prefab Palette: Options")]
     public class PaletteOptionsOverlay : Overlay
     {
+        private Vector2 _scrollPos;
+
         public override VisualElement CreatePanelContent()
         {
             var container = new IMGUIContainer(() =>
             {
-                var tool = PrefabPaletteTool.Instance;
+                _scrollPos = GUILayout.BeginScrollView(_scrollPos, GUILayout.Width(420), GUILayout.Height(250));
 
-                GUILayout.BeginVertical();
-                
-                SceneInteraction.SnapToGrid = GUILayout.Toggle(
-                    SceneInteraction.SnapToGrid,
-                    EditorGUIUtility.IconContent("SceneViewSnap").image,
-                    "Button",
-                    GUILayout.Width(400),
-                    GUILayout.Height(40)
-                );
-                Helpers.DrawLine(Color.grey);
-                // Placement mode
-                GUILayout.Space(4);
-                PlacementModeManager.ToolbarGUI(tool);
-                GUILayout.Space(2.5f);
-                GUILayout.Label(PlacementModeManager.CurrentType.ToString(), EditorStyles.boldLabel);
-                GUILayout.Space(5f);
-                Helpers.DrawLine(Color.grey);
-                PlacementModeManager.CurrentMode.SettingsGUI(tool);
-                GUILayout.Space(10);
-                Helpers.DrawLine(Color.grey);
-                GUILayout.EndVertical();
+                if (EditorWindow.HasOpenInstances<PaletteWindow>())
+                {
+                    var tool = PrefabPaletteTool.Instance;
+
+                    GUILayout.BeginVertical();
+
+                    SceneInteraction.SnapToGrid = GUILayout.Toggle(
+                        SceneInteraction.SnapToGrid,
+                        EditorGUIUtility.IconContent("SceneViewSnap").image,
+                        "Button",
+                        GUILayout.Width(400),
+                        GUILayout.Height(40)
+                    );
+                    Helpers.DrawLine(Color.grey);
+
+                    GUILayout.Space(4);
+                    PlacementModeManager.ToolbarGUI(tool);
+                    GUILayout.Space(2.5f);
+                    GUILayout.Label(PlacementModeManager.CurrentType.ToString(), EditorStyles.boldLabel);
+                    GUILayout.Space(5f);
+
+                    PlacementModeManager.CurrentMode.SettingsGUI(tool);
+
+                    GUILayout.Space(10);
+                    Helpers.DrawLine(Color.grey);
+                    GUILayout.EndVertical();
+                }
+                else
+                {
+                    if (GUILayout.Button("Open Palette"))
+                    {
+                        PaletteWindow.OnShowToolWindow(PrefabPaletteTool.Instance);
+                        SceneView.RepaintAll(); // Attempt to refresh view
+                    }
+                }
+
+                GUILayout.EndScrollView();
             });
 
             return container;
